789403a738c7fcb656c47c8695211bdaab9294bf
haarish04 <haarish.anandan@gmail.com>
2024-06-08 23:58:33 +0530

    Updated closing of sockets

--- src/KVServer.java
+++ src/KVServer.java
@@ -5,14 +5,22 @@ import java.net.Socket;
 public class KVServer{
     private static final int port = 8000;
     private KVStore KVStore = new KVStore();
+    private ServerSocket serverSocket;
 
     public void start() throws IOException{
-        ServerSocket serverSocket= new ServerSocket(port);
+        serverSocket= new ServerSocket(port);
         System.out.println("Server started on port "+ port);
 
         while(true){
             Socket clientSocket = serverSocket.accept();
             new Thread(new ClientHandler(clientSocket, KVStore)).start();
+        }
+    }
+
+    public void shutdown() throws IOException{
+        if(serverSocket != null){
+            serverSocket.close();
+            System.out.println("Server socket closed");
         }
     }
 

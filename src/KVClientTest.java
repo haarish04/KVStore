925a2f14e7ef78567e7ea8caf15aff0342fefe20
haarish04 <haarish.anandan@gmail.com>
2024-06-15 17:10:04 +0530

    Update tests

--- src/KVClientTest.java
+++ src/KVClientTest.java
@@ -5,31 +5,37 @@ public class KVClientTest {
     public static void main(String[] args) {
         KVStore kvStore = new KVStore();
 
+        //Write test to create collection
+
         // Test adding a key-value pair
-        kvStore.set("abc", 100);
-        System.out.println(kvStore.get("abc")); // Expected output: 100
+        kvStore.setRecord("abc", 100);
+        System.out.println(kvStore.getRecord("abc")); // Expected output: 100
 
         // Test retrieving the UUID for a given key
         UUID uuidForAbc = kvStore.getUUIDforKey("abc");
         System.out.println(uuidForAbc); // Expected output: UUID of the key "abc"
 
         // Test updating a key-value pair
-        kvStore.update("abc", 200);
-        System.out.println(kvStore.get("abc")); // Expected output: 200
+        kvStore.updateRecord("abc", 200);
+        System.out.println(kvStore.getRecord("abc")); // Expected output: 200
+
+        //Write test for getAll
 
         // Test deleting a key-value pair
-        if (kvStore.delete("abc")) {
+        if (kvStore.deleteRecord("abc")) {
             System.out.println("Deleted");
         } else {
             System.out.println("Invalid delete");
         }
 
         // Attempt to retrieve a deleted key
-        System.out.println(kvStore.get("abc")); // Expected output: null
+        System.out.println(kvStore.getRecord("abc")); // Expected output: null
 
         // Test adding and deleting a non-existent key
-        if (!kvStore.delete("nonExistentKey")) {
+        if (!kvStore.deleteRecord("nonExistentKey")) {
             System.out.println("Invalid delete");
         }
+
+        //Write test for closing connection
     }
 }
